d11fa72687c7fef3156238b53b784d849ca723a4
yoadwo <yoadw20@gmail.com>
2018-08-18 13:24:47 +0300

    add Data Access Object to work with local MSSQL

--- RedPill-dotnet/App_Start/WebApiConfig.cs
+++ RedPill-dotnet/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -19,6 +20,15 @@ namespace RedPill_dotnet
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // RedPill only works with JSON, so lets remove XML formatting
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // use indented JSON format
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting =
+                           Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
         }
     }
 }
---
+++ RedPill-dotnet/Controllers/PrescriptionsController.cs
@@ -0,0 +1,31 @@
+﻿using PrescriptionsDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RedPill_dotnet.Controllers
+{
+    public class PrescriptionsController : ApiController
+    {
+        //<add name="RedPillEntities" connectionString="metadata=res://*/PrescriptionsDataModel.csdl|res://*/PrescriptionsDataModel.ssdl|res://*/PrescriptionsDataModel.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=YOAD\SQLEXPRESS;initial catalog=RedPill;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework&quot;" providerName="System.Data.EntityClient" />
+
+        public IEnumerable<Prescription> Get()
+        {
+            using (RedPillEntities entities = new RedPillEntities())
+            {
+                return entities.Prescriptions.ToList();
+            }
+        }
+
+        public Prescription Get(int id)
+        {
+            using (RedPillEntities entities = new RedPillEntities())
+            {
+                return entities.Prescriptions.FirstOrDefault(p => p.recordID == id);
+            }
+        }
+    }
+}

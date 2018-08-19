a5e56ebb2cace2d6cc8785e880485795078b5f92
yoadwo <yoadw20@gmail.com>
2018-08-19 15:41:20 +0300

    WriteForm.html - retrieve and present qr code

--- RedPill-dotnet/Controllers/PrescriptionsController.cs
+++ RedPill-dotnet/Controllers/PrescriptionsController.cs
@@ -11,13 +11,16 @@ namespace RedPill_dotnet.Controllers
 {
     public class PrescriptionsController : ApiController
     {
-        [BasicAuthentication]
+
+        public const string PILL_PREFIX = "FSPILLSEN@";
+        //[BasicAuthentication]
         public IEnumerable<Prescription> Get()
         {
-            string docID = System.Threading.Thread.CurrentPrincipal.Identity.Name;
+            //string docID = System.Threading.Thread.CurrentPrincipal.Identity.Name;
             using (RedPillEntities entities = new RedPillEntities())
             {
-                return entities.Prescriptions.Where(doc => doc.docID.Equals(docID)).ToList();
+                return entities.Prescriptions.ToList();
+                //return entities.Prescriptions.Where(doc => doc.docID.Equals(docID)).ToList();
             }
         }
 
@@ -31,6 +34,7 @@ namespace RedPill_dotnet.Controllers
 
         public HttpResponseMessage Post([FromBody] Prescription pre)
         {
+            
             if (pre == null) {
                 var message = Request.CreateErrorResponse(HttpStatusCode.BadRequest,
                     "Prescription object could not be parsed, check request body");
@@ -38,35 +42,46 @@ namespace RedPill_dotnet.Controllers
             }
             try
             {
+                // create response
+                var message = Request.CreateResponse(HttpStatusCode.Created, pre);
+                message.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/png");
+
+                // redirect to the uri of newly created object
+                // does not compile, pres.id is auto generated on server
+                //message.Headers.Location = new Uri(Request.RequestUri +  pre..ToString());
+
                 using (RedPillEntities entities = new RedPillEntities())
                 {
+                    pre.timeAdded = DateTime.Now;
                     // add info to server
                     entities.Prescriptions.Add(pre);
                     entities.SaveChanges();
                 }
                 // create qr code
                 QRCoder.QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
-                QRCoder.QRCodeData qrCodeData = qrGenerator.CreateQrCode(pre.info, QRCoder.QRCodeGenerator.ECCLevel.Q);
+                QRCoder.QRCodeData qrCodeData = qrGenerator.CreateQrCode(PILL_PREFIX + pre.info, QRCoder.QRCodeGenerator.ECCLevel.Q);
+                QRCoder.Base64QRCode qrCode = new QRCoder.Base64QRCode(qrCodeData);
+                string qrCodeImageAsBase64 = qrCode.GetGraphic(20);
+                message.Content = new StringContent(qrCodeImageAsBase64);
+                return message;
+
+                #region bitmap
+                /*
                 QRCoder.QRCode qrCode = new QRCoder.QRCode(qrCodeData);
-
-                // create http response
-                var message = Request.CreateResponse(HttpStatusCode.Created, pre);
-                // redirect to the uri of newly created object
-                // does not compile, pres.id is auto generated on server
-                //message.Headers.Location = new Uri(Request.RequestUri +  pre..ToString());
-
                 using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
                 {
                     using (System.Drawing.Bitmap qrCodeImage = qrCode.GetGraphic(10))
                     {
                         qrCodeImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
                     }
-                    
+
                     // append to message the qr image
                     message.Content = new ByteArrayContent(ms.ToArray());
                     message.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/png");
                 }
-                return message;    
+                return message;
+                */
+                #endregion
             }
             catch (Exception ex)
             {
---
+++ RedPill-dotnet/Scripts/RedPill/write.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+    var pillName = $('#txtName');
+    var totalPills = $('#txtTotalPills');
+    var textStatus = $('#txtStatus');
+    var imgContainer = $('#imgContainer');
+
+    $('#btnWrite').click(function () {
+        var prescriptionEntry = {};
+        prescriptionEntry.docID = 'a';
+        prescriptionEntry.info = JSON.stringify({ "name": pillName.val(), "totalPills": parseInt(totalPills.val()) });
+        prescriptionEntry.patientID = '12';
+        prescriptionEntry.pillName = pillName.val();
+        //datetime is added at server
+        
+        var options = {};
+        options.url = 'api/prescriptions';
+        options.type = 'POST';
+        options.contentType = "application/json";
+        options.accept = 'image/png';
+        options.data = JSON.stringify(prescriptionEntry);
+        options.success = function (imageResults) {
+            console.log("success");
+            //$('#imgContainer').append('<img src="' + imageResults + '" />');
+            var config = 'data:image/png;base64,';
+            //imgContainer.append('img src="' + config + imageResults + '"/>');
+            imgContainer.attr("src", config + imageResults);
+            
+            
+        };
+        options.error = function (err) {
+            console.log("error");
+        };
+
+        $.ajax(options);
+
+    });
+
+    $('#btnClear').click(function () {
+        console.log("clear");
+        pillName.val("");
+        totalPills.val("");
+        imgContainer.attr('src', "");
+
+    });
+});
